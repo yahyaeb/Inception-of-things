c58d3c2619b57bbb0403a1dd9fa51b4e9802d218
yahyaeb <yahya.elboukili1@gmail.com>
2026-06-18 15:58:51 +0200

    part 1 completed | added a run_vagrant.sh cleaner to run vagrant fresh from scratch. configure globl commands and fixed the machine to run on Qemu

---
+++ run_vagrant.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Cleanup
+sudo virsh destroy yel-boukS 2>/dev/null
+sudo virsh undefine yel-boukS 2>/dev/null
+sudo virsh destroy yel-boukSW 2>/dev/null
+sudo virsh undefine yel-boukSW 2>/dev/null
+sudo virsh vol-delete --pool default yel-boukS.img 2>/dev/null
+sudo virsh vol-delete --pool default yel-boukSW.img 2>/dev/null
+sudo virsh net-destroy k3s-net 2>/dev/null
+sudo virsh net-undefine k3s-net 2>/dev/null
+rm -rf .vagrant/
+
+# Start
+vagrant up --provider=libvirt
