81b2a64a4a7a3867fe661d99e1be3c7dc6fbbe82
yahyaeb <yahya.elboukili1@gmail.com>
2026-06-24 17:30:34 +0200

    P2 in progress. Created 3 pods needed and need to work on the ingress conf

---
+++ p2/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo virsh destroy yel-boukS 2>/dev/null
+sudo virsh undefine yel-boukS 2>/dev/null
+sudo virsh destroy yel-boukSW 2>/dev/null
+sudo virsh undefine yel-boukSW 2>/dev/null
+sudo virsh vol-delete --pool default yel-boukS.img 2>/dev/null
+sudo virsh vol-delete --pool default yel-boukSW.img 2>/dev/null
+sudo exportfs -ra
+rm -rf .vagrant/
